fe10973b3001765408c3e5d233cf493fc2f41822
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-08-17 19:43:51 +0530

    Riding service completed

--- ride-service/src/main/java/com/rideshare/rideservice/config/KafkaConfig.java
+++ ride-service/src/main/java/com/rideshare/rideservice/config/KafkaConfig.java
@@ -1,4 +1,32 @@
 package com.rideshare.rideservice.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+    // Topic where Ride service published ride request
+    // Matching service subscribers to this topic
+
+    @Bean
+    public NewTopic rideRequestedTopic(){
+        return TopicBuilder.name("ride.requested")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    // Topic where matching service published match results
+    // Ride Service subscribers to this topic
+
+    @Bean
+    public NewTopic rideMatchedTopic(){
+        return TopicBuilder.name("ride.matched")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
 }
--- ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
+++ ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
@@ -11,6 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -71,6 +75,78 @@ public class RideService {
         ride.setDriverId(driverId);
         ride.setStatus(RideStatus.ACCEPTED);
         rideRepository.save(ride);
+    }
+
+    public RideResponse startRide(String rideId){
+        Ride ride = rideRepository.findById(rideId).orElseThrow(() -> new RuntimeException("Ride not found"));
+
+        if(ride.getStatus() != RideStatus.ACCEPTED){
+            throw new RuntimeException("Ride cannot started. current status is: " + ride.getStatus());
+        }
+
+        ride.setStatus(RideStatus.RIDE_STARTED);
+        ride.setStartedAt(LocalDateTime.now());
+        rideRepository.save(ride);
+
+        return mapToResponse(ride);
+    }
+
+    public RideResponse completeRide(String rideId){
+        Ride ride = rideRepository.findById(rideId).orElseThrow(() -> new RuntimeException("Ride not found"));
+
+        if(ride.getStatus() != RideStatus.RIDE_STARTED){
+            throw new RuntimeException("Ride cannot be complete. current status is: " + ride.getStatus());
+        }
+
+        ride.setStatus(RideStatus.COMPLETED);
+        ride.setCompletedAt(LocalDateTime.now());
+        ride.setActualFare(ride.getEstimatedFare());
+        rideRepository.save(ride);
+
+        return mapToResponse(ride);
+    }
+
+    public RideResponse cancelRide(String rideId){
+        Ride ride = rideRepository.findById(rideId).orElseThrow(() -> new RuntimeException("Ride not found"));
+
+        ride.setStatus(RideStatus.CANCELLED);
+        rideRepository.save(ride);
+        return mapToResponse(ride);
+    }
+
+    public RideResponse getRideById(String rideId){
+        Ride ride = rideRepository.findById(rideId).orElseThrow(() -> new RuntimeException("Ride not found"));
+
+        return mapToResponse(ride);
+    }
+
+    public List<RideResponse> getRidesByRider(String riderId){
+        List<Ride> ride = rideRepository.findByRiderIdOrderByCreatedAtDesc(riderId);
+
+        return ride.stream().map(this::mapToResponse).collect(Collectors.toList());
+    }
+
+    private double calculateEstimateFare(RideRequest rideRequest){
+        // Simplified Haversine distance calculation
+
+        double lat1 = Math.toRadians(rideRequest.getPickupLatitude());
+        double long1 = Math.toRadians(rideRequest.getPickupLongitude());
+        double lat2 = Math.toRadians(rideRequest.getDropLatitude());
+        double long2 = Math.toRadians(rideRequest.getDropLongitude());
+
+        double dLat = lat1 - lat2;
+        double dLong = long1 - long2;
+
+        double a = Math.pow(Math.sin(dLat/2),2)
+                   +Math.cos(lat1) * Math.cos(lat2)
+                   *Math.pow(Math.sin(dLong/2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double distanceInKm = 6371 * c;
+
+        //Base fare: 50Rs + 12Rs per km
+        double fare = 50 + (distanceInKm * 12);
+        return Math.round(fare * 100.0) / 100.0;
     }
 
     public RideResponse mapToResponse(Ride savedRide){
